61258b6e3b8043369ab70919644551e0e2af6d0c
Kebi <kevikostandini@gmail.com>
2023-10-21 13:12:55 +0200

    fixed register

--- custom_auth/serializers.py
+++ custom_auth/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
@@ -6,21 +5,22 @@ from rest_framework.validators import UniqueValidator
 from exceptions import InvalidArgumentException
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+from django.contrib.auth import get_user_model
 User = get_user_model()
-
 class RegisterSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(required=True, source="first_name")
-    last_name = serializers.CharField(required=True, source="last_name")
+    first_name = serializers.CharField(required=True,)
+    last_name = serializers.CharField(required=True,)
     email = serializers.EmailField(
         required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
-
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    date_of_birth = serializers.DateField(required=True)  # Add date_of_birth field
+
     class Meta:
         model = User
-        fields = ('password', 'password2', 'email', 'firstName', 'lastName')
+        fields = ('password', 'password2', 'email', 'first_name', 'last_name', 'date_of_birth')
         extra_kwargs = {
             'first_name': {'required': True},
             'last_name': {'required': True}
@@ -37,6 +37,8 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
+            password=validated_data['password'],
+            date_of_birth=validated_data['date_of_birth'],
         )
 
         user.set_password(validated_data['password'])
